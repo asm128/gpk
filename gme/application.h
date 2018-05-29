6e142d3b7fd13bc143fc17ba42b07f8bb9b29a2a
asm128 <pabloarielz@outlook.com>
2018-05-29 19:33:17 -0300

    + Added exit button to gme.

--- gme/application.h
+++ gme/application.h
@@ -14,6 +14,8 @@ namespace gme // I'm gonna use a different namespace in order to test a few thin
 		::gpk::STexture<::gpk::SColorBGRA>		TextureFont							= {};
 		::gpk::ptr_obj<::gpk::SRenderTarget>	Offscreen							= {};
 
+		int32_t									IdExit								= -1;
+
 		::std::mutex							LockGUI;
 		::std::mutex							LockRender;
 
