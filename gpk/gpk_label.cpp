b2d119d137b3499cb8b3a1a64e426d819a7fd100
asm128 <pabloarielz@outlook.com>
2018-07-01 20:40:49 -0300

    + Removed 32% of the lines in gpk_label.cpp.

--- gpk/gpk_label.cpp
+++ gpk/gpk_label.cpp
@@ -1,30 +1,20 @@
 #include "gpk_label.h"
 #include "gpk_label_manager.h"
 
-static	::gpk::CLabelManager*				labelManagerSingleton					()													noexcept	{
-	static	::gpk::CLabelManager					instanceLabelManager;
-	return &instanceLabelManager;
-}
+static	::gpk::CLabelManager*				labelManagerSingleton						()													noexcept	{ static ::gpk::CLabelManager instanceLabelManager; return &instanceLabelManager; }
 
-											::gpk::label::label						(const char* text, uint32_t stringLen)				noexcept	: LabelManager(labelManagerSingleton())
-{
-	LabelManager->ArrayView(text, stringLen, *this);	
-}
+											::gpk::label::label							(const char* text, uint32_t stringLen)				noexcept	: LabelManager(::labelManagerSingleton())			{ LabelManager->ArrayView(text, stringLen, *this); }
 
-				bool						gpk::label::operator==					(const label& other)						const	noexcept	{
+		bool								::gpk::label::operator==					(const label& other)						const	noexcept	{
 	if(Data == other.Data)
 		return true;
 	if(Count != other.Count)
 		return false;
-	if(LabelManager == other.LabelManager)
-		return false;
-	return 0 == memcmp(Data, other.Data, Count);
+	return (LabelManager == other.LabelManager) ? false : 0 == memcmp(Data, other.Data, Count);
 }
 
-				bool						gpk::label::operator==					(const ::gpk::view_const_string& other)		const	noexcept	{
+		bool								::gpk::label::operator==					(const ::gpk::view_const_string& other)		const	noexcept	{
 	if(Data == other.begin()) 
 		return true;
-	if(Count != other.size())
-		return false;
-	return 0 == memcmp(Data, other.begin(), Count);
+	return (Count != other.size()) ? false : 0 == memcmp(Data, other.begin(), Count);
 }
