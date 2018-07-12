43ae1b2096d840c23245b4bb09eb7e4cdf6df3c2
asm128 <pabloarielz@outlook.com>
2018-07-11 23:14:03 -0300

    + Added tcpipAddress() helpers.

--- gpk/gpk_endpoint_command.h
+++ gpk/gpk_endpoint_command.h
@@ -8,22 +8,26 @@ namespace gpk
 #pragma pack(push, 1)
 	enum ENDPOINT_COMMAND : uint8_t
 		{ ENDPOINT_COMMAND_NOOP				= 0
-		, ENDPOINT_COMMAND_DISCONNECT
-		, ENDPOINT_COMMAND_CONNECT
-		, ENDPOINT_COMMAND_PING
-		, ENDPOINT_COMMAND_TIME
-		, ENDPOINT_COMMAND_PAYLOAD
+		, ENDPOINT_COMMAND_DISCONNECT		= 1
+		, ENDPOINT_COMMAND_CONNECT			= 2
+		, ENDPOINT_COMMAND_PING				= 3
+		, ENDPOINT_COMMAND_TIME				= 4
+		, ENDPOINT_COMMAND_PAYLOAD			= 5
+		, ENDPOINT_COMMAND_UNUSED_0			= 6
+		, ENDPOINT_COMMAND_INVALID			= 7
 		};
 
 	enum ENDPOINT_MESSAGE_TYPE : uint8_t
-		{ ENDPOINT_MESSAGE_TYPE_REQUEST		= 1
+		{ ENDPOINT_MESSAGE_TYPE_REQUEST		= 0
 		, ENDPOINT_MESSAGE_TYPE_RESPONSE
 		};
 
 	struct SEndpointCommand {
-		ENDPOINT_COMMAND		Command		: 6;
-		ENDPOINT_MESSAGE_TYPE	Type		: 2;
+		ENDPOINT_COMMAND		Command			: 3;
+		ENDPOINT_MESSAGE_TYPE	Type			: 1;
+		uint8_t					PayloadBytes	: 4;
 	};
+	//constexpr const size_t etlaskn = sizeof(SEndpointCommand);
 #pragma pack(pop)
 } // namespace
 
