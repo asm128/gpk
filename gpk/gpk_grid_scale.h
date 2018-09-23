573493545f477a9d1f29186ace63c37d9cd9daec
asm128 <pabloarielz@outlook.com>
2018-09-23 10:36:36 -0300

    * Fixed connection address. * Fixed grid_scale() outdated code. - Modified hardcoded value to process 100 Windows messages per frame.

--- gpk/gpk_grid_scale.h
+++ gpk/gpk_grid_scale.h
@@ -7,30 +7,30 @@ namespace gpk
 {
 	template<typename _tCell>
 						::gpk::error_t							grid_scale_fast					(::gpk::view_grid<_tCell>& dst, const ::gpk::view_grid<_tCell>& src, const float xFactor, const float yFactor)		{
-		for(uint32_t y = 0; y < dst.height(); ++y) 
-			for(uint32_t x = 0; x < dst.width(); ++x) 
-				dst[y][x]													= src[(uint32_t)(y * yFactor)][(uint32_t)(x * xFactor)];
+		for(uint32_t y = 0; y < dst.metrics().y; ++y) 
+		for(uint32_t x = 0; x < dst.metrics().x; ++x) 
+			dst[y][x]													= src[(uint32_t)(y * yFactor)][(uint32_t)(x * xFactor)];
 		return 0;
 	}
 
 	template<typename _tCell>
 						::gpk::error_t							grid_scale					(::gpk::view_grid<_tCell>& dst, const ::gpk::view_grid<_tCell>& src)													{
-		const float														xFactor						= src.width () / (float)dst.width ();
-		const float														yFactor						= src.height() / (float)dst.height();
+		const float														xFactor						= src.metrics().x / (float)dst.metrics().x;
+		const float														yFactor						= src.metrics().y / (float)dst.metrics().y;
 		return grid_scale_fast(dst, src, xFactor, yFactor);
 	}
 
 	template<typename _tCell>
 						::gpk::error_t							grid_scale					(::gpk::view_grid<_tCell>& dst, const ::gpk::view_grid<_tCell>& src, const float xFactor, const float yFactor)			{
-		for(uint32_t y = 0; y < dst.height(); ++y) 
-			for(uint32_t x = 0; x < dst.width(); ++x) { 
-				const uint32_t													xSrc						= (uint32_t)(x * yFactor);
-				const uint32_t													ySrc						= (uint32_t)(y * xFactor);
-				if( ::gpk::in_range(ySrc, 0, src.height())
-				 && ::gpk::in_range(xSrc, 0, src.width ())
-				 )
-					dst[y][x]													= src[ySrc][xSrc];
-			}
+		for(uint32_t y = 0; y < dst.metrics().y; ++y) 
+		for(uint32_t x = 0; x < dst.metrics().x; ++x) { 
+			const uint32_t													xSrc						= (uint32_t)(x * yFactor);
+			const uint32_t													ySrc						= (uint32_t)(y * xFactor);
+			if( ::gpk::in_range(ySrc, 0, src.metrics().y)
+			 && ::gpk::in_range(xSrc, 0, src.metrics().x)
+			 )
+				dst[y][x]													= src[ySrc][xSrc];
+		}
 		return 0;
 	}
 
