638604e65ecc2eaa0173f627ca2d9906d162dea0
asm128 <pabloarielz@outlook.com>
2019-06-09 13:03:01 -0300

    + Added pathList() inline overload. - Removed useless file that came from nowhere.

--- gpk/gpk_adam7.h
+++ gpk/gpk_adam7.h
@@ -15,13 +15,13 @@ namespace gpk
 	template<typename _tTexel>
 	static			::gpk::error_t											adam7Interlace					(::gpk::view_array<::gpk::SImage<_tTexel>> images, ::gpk::view_grid<_tTexel> & out_View)				{
 		for(uint32_t iImage = 0; iImage < images.size(); ++iImage) {
-			::gpk::SCoord2<uint32_t>													offsetMultiplier								= {1, 1};
-			::gpk::SCoord2<uint32_t>													offsetBase										= {0, 0};
+			::gpk::SCoord2<uint32_t>													offsetMultiplier				= {1, 1};
+			::gpk::SCoord2<uint32_t>													offsetBase						= {0, 0};
 			gpk_necall(::gpk::adam7ScaleIndex(iImage, offsetMultiplier, offsetBase), "%s", "Invalid Adam7 image?");
-			::gpk::SImage<_tTexel>														& image											= images[iImage];
+			::gpk::SImage<_tTexel>														& image							= images[iImage];
 			for(uint32_t y = 0; y < image.metrics().y; ++y)
 			for(uint32_t x = 0; x < image.metrics().x; ++x) {
-				::gpk::SCoord2<uint32_t>													targetCell										= {x * offsetMultiplier.x + offsetBase.x, y * offsetMultiplier.y + offsetBase.y};
+				::gpk::SCoord2<uint32_t>													targetCell						= {x * offsetMultiplier.x + offsetBase.x, y * offsetMultiplier.y + offsetBase.y};
 				if(targetCell.y < out_View.metrics().y)
 					if(targetCell.x < out_View[targetCell.y].size())
 						out_View[targetCell.y][targetCell.x]									= image.View[y][x];
--- gpk/gpk_storage.h
+++ gpk/gpk_storage.h
@@ -6,20 +6,25 @@
 
 namespace gpk
 {
-		struct SPathContents {
-					::gpk::array_obj<::gpk::array_pod<char_t>>							Files							= {};
-					::gpk::array_obj<::gpk::SPathContents>								Folders							= {};
-		};
+	struct SPathContents {
+						::gpk::array_obj<::gpk::array_pod<char_t>>		Files					= {};
+						::gpk::array_obj<::gpk::SPathContents>			Folders					= {};
+	};
 
-				::gpk::error_t														pathList						(const ::gpk::SPathContents& input, ::gpk::array_obj<::gpk::array_pod<char_t>>& output);	// recursively walk over a pathcontents hierarchy and store all the file names into "output"
-				::gpk::error_t														pathList						(const ::gpk::view_const_string& pathToList, SPathContents & out_Contents);		// Recursive
-				::gpk::error_t														pathList						(const ::gpk::view_const_string& pathToList, ::gpk::array_obj<::gpk::array_pod<char_t>>& output, bool listFolders);		// Not recursive
-				::gpk::error_t														fileToMemory					(const ::gpk::view_const_string& fileName, ::gpk::array_pod<byte_t>& fileInMemory);
-				::gpk::error_t														fileFromMemory					(const ::gpk::view_const_string& fileName, const ::gpk::array_pod<byte_t>& fileInMemory);
-				::gpk::error_t														dirCreate						(const ::gpk::view_const_string& folderName);	// Recursive
-				::gpk::error_t														fileJoin						(const ::gpk::view_const_string	& fileNameDst);	// Joins a file split into file.split.## parts.
-				::gpk::error_t														fileSplit						(const ::gpk::view_const_string	& fileNameSrc);	// Splits a file into file.split.## parts.
-
+					::gpk::error_t									pathList				(const ::gpk::SPathContents& input, ::gpk::array_obj<::gpk::array_pod<char_t>>& output);	// recursively walk over a pathcontents hierarchy and store all the file names into "output"
+					::gpk::error_t									pathList				(const ::gpk::view_const_string& pathToList, SPathContents & out_Contents);		// Recursive
+	static inline	::gpk::error_t									pathList				(const ::gpk::view_const_string& pathToList, ::gpk::array_obj<::gpk::array_pod<char_t>>& output) {
+		::gpk::SPathContents												tree					= {};
+		gpk_necall(::gpk::pathList(pathToList, tree), "%s", "?");
+		gpk_necall(::gpk::pathList(tree, output), "%s", "?");
+		return 0;
+	}	// Recursive
+					::gpk::error_t									pathList				(const ::gpk::view_const_string& pathToList, ::gpk::array_obj<::gpk::array_pod<char_t>>& output, bool listFolders);		// Not recursive
+					::gpk::error_t									fileToMemory			(const ::gpk::view_const_string& fileName, ::gpk::array_pod<byte_t>& fileInMemory);
+					::gpk::error_t									fileFromMemory			(const ::gpk::view_const_string& fileName, const ::gpk::array_pod<byte_t>& fileInMemory);
+					::gpk::error_t									dirCreate				(const ::gpk::view_const_string& folderName);	// Recursive
+					::gpk::error_t									fileJoin				(const ::gpk::view_const_string	& fileNameDst);	// Joins a file split into file.split.## parts.
+					::gpk::error_t									fileSplit				(const ::gpk::view_const_string	& fileNameSrc);	// Splits a file into file.split.## parts.
 } // namespace
 
 #endif // GPK_STORAGE_H_2983749283
