b73b6d7f5baf149f6d52649d71188ec704be7448
asm128 <pabloarielz@outlook.com>
2019-06-22 03:44:23 -0300

    + Added code for formatting a string from a JSON object value through a basic expression.

--- gpk/gpk_json_formatter.cpp
+++ gpk/gpk_json_formatter.cpp
@@ -15,48 +15,76 @@
 	return 0;
 }
 
-::gpk::error_t									jsonStringFormatResolve					(const ::gpk::view_const_string & expression, const ::gpk::SJSONNode& input, ::gpk::array_pod<char_t>& output)													{
+::gpk::error_t									evaluateNode							(const ::gpk::SExpressionNode* nodeExpression, const ::gpk::view_array<::gpk::view_const_string>& viewsExpression, const ::gpk::SJSONNode& input, const ::gpk::view_array<::gpk::view_const_string>& viewsJSON, ::gpk::view_const_string& output)						{
+	if(0 == nodeExpression->Children.size()) {
+		if(nodeExpression->Object->Type == ::gpk::EXPRESSION_READER_TYPE_KEY) {
+			ree_if(input.Object->Type != ::gpk::JSON_TYPE_OBJECT, "Only objects can be accessed by key. JSON type: %s.", ::gpk::get_value_label(input.Object->Type).begin());
+			int32_t												jsonIndex								= ::gpk::jsonObjectValueGet(input, viewsJSON, viewsExpression[nodeExpression->ObjectIndex]);
+			output											= viewsJSON[jsonIndex];
+			return jsonIndex;
+		}
+		else if(nodeExpression->Object->Type == ::gpk::EXPRESSION_READER_TYPE_INDEX) {
+			ree_if(input.Object->Type != ::gpk::JSON_TYPE_ARRAY, "Only arrays can be accessed by key. JSON type: %s.", ::gpk::get_value_label(input.Object->Type).begin());
+			uint64_t											numberRead								= 0;
+			::gpk::parseArbitraryBaseInteger(10, "0123456789", viewsJSON[input.ObjectIndex], &numberRead);
+			int32_t												jsonIndex								= ::gpk::jsonArrayValueGet(input, (uint32_t)numberRead);
+			output											= viewsJSON[jsonIndex];
+			return jsonIndex;
+		}
+	}
+	else
+		for(uint32_t iChild = 0; iChild < nodeExpression->Children.size(); ++iChild) {
+			gpk_necall(::evaluateNode(nodeExpression->Children[iChild], viewsExpression, input, viewsJSON, output), "Failed to evaluate node at index %i.", nodeExpression->Children[iChild]->ObjectIndex);
+		}
+	return 0;
+}
+
+::gpk::error_t									jsonStringFormatResolve					(const ::gpk::view_const_string & expression, const ::gpk::SJSONNode& input, const ::gpk::view_array<::gpk::view_const_string>& viewsJSON, ::gpk::view_const_string& output)		{
 	::gpk::view_const_string							resultOfExpressionEval					= {};
 	::gpk::SExpressionReader							reader;
-	::gpk::expressionReaderParse(reader, expression);
+	gpk_necall(::gpk::expressionReaderParse(reader, expression), "Failed to read JSONeN expression: '%s'.", expression.begin());
 	const ::gpk::array_obj<::gpk::view_const_string>	& expressionViews						= reader.View;
 	for(uint32_t iView = 0; iView < expressionViews.size(); ++iView) {
 		const ::gpk::view_const_string						& viewExpression						= expressionViews[iView];
 		const ::gpk::SExpressionReaderType					& typeExpression						= reader.Object[iView];
 		if(viewExpression.size()) {
-			uint32_t										lenString									= viewExpression.size();
-			char											bufferFormat [8192]							= {};
+			uint32_t											lenString								= viewExpression.size();
+			char												bufferFormat [8192]						= {};
 			sprintf_s(bufferFormat, "Expression element: %%.%us. Type: %s. Parent: %i. Begin: %u. End: %u.", lenString, ::gpk::get_value_label(typeExpression.Type).begin(), typeExpression.ParentIndex, typeExpression.Span.Begin, typeExpression.Span.End);
 			info_printf(bufferFormat, viewExpression.begin());
 		}
 	}
-	if(reader.Tree.size())
-		::printNode(reader.Tree[0], expression);
-	// TODO: Implement expression evaluation.
 	::gpk::view_const_string							evaluated								= {};
+	if(reader.Tree.size()) {
+		::printNode(reader.Tree[0], expression);
+		gpk_necall(::evaluateNode(reader.Tree[0], expressionViews, input, viewsJSON, output), "Failed to evaluate expression: '%s'", expression.begin());
+		evaluated										= output;
+	}
+	
+	// TODO: Implement expression evaluation.
+
 	if(evaluated.size()) {
 		uint32_t											lenString								= evaluated.size();
 		char												bufferFormat [8192]						= {};
 		sprintf_s(bufferFormat, "Result of expression evaluation: %%.%us.", lenString);
 		info_printf(bufferFormat, evaluated.begin());
 	}
-	output, input;
 	return 0;
 }
 
-::gpk::error_t									gpk::jsonStringFormat					(const ::gpk::view_const_string& format, const ::gpk::SJSONNode& input, ::gpk::array_pod<char_t>& output)													{
+::gpk::error_t									gpk::jsonStringFormat					(const ::gpk::view_const_string& format, const ::gpk::SJSONNode& input, const ::gpk::view_array<::gpk::view_const_string>& viewsJSON, ::gpk::array_pod<char_t>& output)													{
 	::gpk::SStripLiteralState							stateLiteralStripper					= {};
 	::gpk::array_pod<::gpk::SStripLiteralType>			typesLiteral							= {};
 	::gpk::stripLiteralParse(stateLiteralStripper, typesLiteral, format);	// strip root literals 
 	::gpk::array_obj<::gpk::view_const_string>			views									= {};
 	::gpk::stripLiteralGetViews(views, typesLiteral, format);
 
-	::gpk::array_obj<::gpk::array_pod<char_t>>			resolved;
-	resolved.resize(views.size());
 	info_printf("-**- %s -**-", "Results of token search");
 	for(uint32_t iView = 0; iView < views.size(); ++iView) {
-		if(typesLiteral[iView].Type == ::gpk::STRIP_LITERAL_TYPE_TOKEN)	// we only have to solve tokens
-			::jsonStringFormatResolve(views[iView], input, resolved[iView]);
+		if(typesLiteral[iView].Type == ::gpk::STRIP_LITERAL_TYPE_TOKEN)	{ // we only have to solve tokens
+			::gpk::view_const_string						& toResolve								= views[iView];
+			gpk_necall(::jsonStringFormatResolve(views[iView], input, viewsJSON, toResolve), "Failed to resolve expression: '%s'", views[iView].begin());
+		}
 	}
 	for(uint32_t iView = 0; iView < views.size(); ++iView)
 		output.append(views[iView]);
--- gpk/gpk_json_formatter.h
+++ gpk/gpk_json_formatter.h
@@ -5,7 +5,7 @@
 
 namespace gpk
 {
-	::gpk::error_t								jsonStringFormat			(const ::gpk::view_const_string& format, const ::gpk::SJSONNode& input, ::gpk::array_pod<char_t>& output);
+	::gpk::error_t								jsonStringFormat			(const ::gpk::view_const_string& format, const ::gpk::SJSONNode& inputJSON, const ::gpk::view_array<::gpk::view_const_string>& viewsJSON, ::gpk::array_pod<char_t>& output);
 } // namespace
 
 #endif // GPK_JSON_FORMATTER_923874928374
