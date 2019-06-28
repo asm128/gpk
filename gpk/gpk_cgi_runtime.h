917680029014b81ca2b98c2dc513701aeb5bc7b3
asm128 <pabloarielz@outlook.com>
2019-06-27 22:47:32 -0300

    * Added missing member registry.

--- gpk/gpk_cgi_runtime.h
+++ gpk/gpk_cgi_runtime.h
@@ -7,42 +7,6 @@
 
 namespace gpk
 {
-	enum CGI_MEDIA_TYPE
-		{ CGI_MEDIA_TYPE_APPLICATION_JAVASCRIPT															// application/javascript
-		, CGI_MEDIA_TYPE_APPLICATION_JSON																// application/json
-		, CGI_MEDIA_TYPE_APPLICATION_X_WWW_FORM_URLENCODED												// application/x-www-form-urlencoded
-		, CGI_MEDIA_TYPE_APPLICATION_XML																// application/xml
-		, CGI_MEDIA_TYPE_APPLICATION_ZIP																// application/zip
-		, CGI_MEDIA_TYPE_APPLICATION_PDF																// application/pdf
-		, CGI_MEDIA_TYPE_APPLICATION_SQL																// application/sql
-		, CGI_MEDIA_TYPE_APPLICATION_GRAPHQL															// application/graphql
-		, CGI_MEDIA_TYPE_APPLICATION_LD_JSON															// application/ld+json
-		, CGI_MEDIA_TYPE_APPLICATION_MSWORD_DOC															// application/msword (.doc)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT_DOCX	// application/vnd.openxmlformats-officedocument.wordprocessingml.document(.docx)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_MS_EXCEL_XLS													// application/vnd.ms-excel (.xls)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET_XLSX			// application/vnd.openxmlformats-officedocument.spreadsheetml.sheet (.xlsx)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_MS_POWERPOINT_PPT												// application/vnd.ms-powerpoint (.ppt)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION_PPTX	// application/vnd.openxmlformats-officedocument.presentationml.presentation (.pptx)
-		, CGI_MEDIA_TYPE_APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT_ODT									// application/vnd.oasis.opendocument.text (.odt)
-		, CGI_MEDIA_TYPE_AUDIO_MPEG																		// audio/mpeg
-		, CGI_MEDIA_TYPE_AUDIO_OGG																		// audio/ogg
-		, CGI_MEDIA_TYPE_MULTIPART_FORM_DATA															// multipart/form-data
-		, CGI_MEDIA_TYPE_TEXT_CSS																		// text/css
-		, CGI_MEDIA_TYPE_TEXT_HTML																		// text/html
-		, CGI_MEDIA_TYPE_TEXT_XML																		// text/xml
-		, CGI_MEDIA_TYPE_TEXT_CSV																		// text/csv
-		, CGI_MEDIA_TYPE_TEXT_PLAIN																		// text/plain
-		, CGI_MEDIA_TYPE_IMAGE_PNG																		// image/png
-		, CGI_MEDIA_TYPE_IMAGE_JPEG																		// image/jpeg
-		, CGI_MEDIA_TYPE_IMAGE_GIF																		// image/gif
-		};
-
-	struct SCGIRequestContent {
-		uint32_t																				Length						= {};
-		::gpk::CGI_MEDIA_TYPE																	Type						= {};
-		::gpk::array_pod<char>																	Body						= {};
-	};
-
 	struct SCGIEnvironment {
 		GPKMOND(::gpk, array_pod<char_t>,	AUTH_PASSWORD			) = "";
 		GPKMOND(::gpk, array_pod<char_t>,	AUTH_TYPE				) = "";
@@ -92,7 +56,92 @@ namespace gpk
 		GPKMOND(::gpk, array_pod<char_t>,	REQUEST_URI				) = "";// The interpreted pathname of the requested document or CGI (relative to the document root)
 		GPKMOND(::gpk, array_pod<char_t>,	SCRIPT_FILENAME			) = "";// The full pathname of the current CGI
 		GPKMOND(::gpk, array_pod<char_t>,	SERVER_ADMIN			) = "";// The email address for your server's webmaster
-		GPKM_REGISTRY()
+		GPKM_REGISTRY
+			(	GPKM_NAME(AUTH_PASSWORD			)
+			,	GPKM_NAME(AUTH_TYPE				)
+			,	GPKM_NAME(AUTH_USER				)
+			,	GPKM_NAME(CERT_COOKIE			)
+			,	GPKM_NAME(CERT_FLAGS			)
+			,	GPKM_NAME(CERT_ISSUER			)
+			,	GPKM_NAME(CERT_KEYSIZE			)
+			,	GPKM_NAME(CERT_SECRETKEYSIZE	)
+			,	GPKM_NAME(CERT_SERIALNUMBER		)
+			,	GPKM_NAME(CERT_SERVER_ISSUER	)
+			,	GPKM_NAME(CERT_SERVER_SUBJECT	)
+			,	GPKM_NAME(CERT_SUBJECT			)
+			,	GPKM_NAME(CF_TEMPLATE_PATH		)
+			,	GPKM_NAME(CONTENT_LENGTH		)
+			,	GPKM_NAME(CONTENT_TYPE			)
+			,	GPKM_NAME(CONTEXT_PATH			)
+			,	GPKM_NAME(GATEWAY_INTERFACE		)
+			,	GPKM_NAME(HTTPS					)
+			,	GPKM_NAME(HTTPS_KEYSIZE			)
+			,	GPKM_NAME(HTTPS_SECRETKEYSIZE	)
+			,	GPKM_NAME(HTTPS_SERVER_ISSUER	)
+			,	GPKM_NAME(HTTPS_SERVER_SUBJECT	)
+			,	GPKM_NAME(HTTP_ACCEPT			)
+			,	GPKM_NAME(HTTP_ACCEPT_ENCODING	)
+			,	GPKM_NAME(HTTP_ACCEPT_LANGUAGE	)
+			,	GPKM_NAME(HTTP_CONNECTION		)
+			,	GPKM_NAME(HTTP_COOKIE			)
+			,	GPKM_NAME(HTTP_HOST				)
+			,	GPKM_NAME(HTTP_REFERER			)
+			,	GPKM_NAME(HTTP_USER_AGENT		)
+			,	GPKM_NAME(QUERY_STRING			)
+			,	GPKM_NAME(REMOTE_ADDR			)
+			,	GPKM_NAME(REMOTE_HOST			)
+			,	GPKM_NAME(REMOTE_USER			)
+			,	GPKM_NAME(REQUEST_METHOD		)
+			,	GPKM_NAME(SCRIPT_NAME			)
+			,	GPKM_NAME(SERVER_NAME			)
+			,	GPKM_NAME(SERVER_PORT			)
+			,	GPKM_NAME(SERVER_PORT_SECURE	)
+			,	GPKM_NAME(SERVER_PROTOCOL		)
+			,	GPKM_NAME(SERVER_SOFTWARE		)
+			,	GPKM_NAME(WEB_SERVER_API		)
+			,	GPKM_NAME(DOCUMENT_ROOT			)
+			,	GPKM_NAME(PATH					)
+			,	GPKM_NAME(REMOTE_PORT			)
+			,	GPKM_NAME(REQUEST_URI			)
+			,	GPKM_NAME(SCRIPT_FILENAME		)
+			,	GPKM_NAME(SERVER_ADMIN			)
+			);
+	};
+
+	enum CGI_MEDIA_TYPE
+		{ CGI_MEDIA_TYPE_APPLICATION_JAVASCRIPT															// application/javascript
+		, CGI_MEDIA_TYPE_APPLICATION_JSON																// application/json
+		, CGI_MEDIA_TYPE_APPLICATION_X_WWW_FORM_URLENCODED												// application/x-www-form-urlencoded
+		, CGI_MEDIA_TYPE_APPLICATION_XML																// application/xml
+		, CGI_MEDIA_TYPE_APPLICATION_ZIP																// application/zip
+		, CGI_MEDIA_TYPE_APPLICATION_PDF																// application/pdf
+		, CGI_MEDIA_TYPE_APPLICATION_SQL																// application/sql
+		, CGI_MEDIA_TYPE_APPLICATION_GRAPHQL															// application/graphql
+		, CGI_MEDIA_TYPE_APPLICATION_LD_JSON															// application/ld+json
+		, CGI_MEDIA_TYPE_APPLICATION_MSWORD_DOC															// application/msword (.doc)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT_DOCX	// application/vnd.openxmlformats-officedocument.wordprocessingml.document(.docx)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_MS_EXCEL_XLS													// application/vnd.ms-excel (.xls)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_SPREADSHEETML_SHEET_XLSX			// application/vnd.openxmlformats-officedocument.spreadsheetml.sheet (.xlsx)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_MS_POWERPOINT_PPT												// application/vnd.ms-powerpoint (.ppt)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_OPENXMLFORMATS_OFFICEDOCUMENT_PRESENTATIONML_PRESENTATION_PPTX	// application/vnd.openxmlformats-officedocument.presentationml.presentation (.pptx)
+		, CGI_MEDIA_TYPE_APPLICATION_VND_OASIS_OPENDOCUMENT_TEXT_ODT									// application/vnd.oasis.opendocument.text (.odt)
+		, CGI_MEDIA_TYPE_AUDIO_MPEG																		// audio/mpeg
+		, CGI_MEDIA_TYPE_AUDIO_OGG																		// audio/ogg
+		, CGI_MEDIA_TYPE_MULTIPART_FORM_DATA															// multipart/form-data
+		, CGI_MEDIA_TYPE_TEXT_CSS																		// text/css
+		, CGI_MEDIA_TYPE_TEXT_HTML																		// text/html
+		, CGI_MEDIA_TYPE_TEXT_XML																		// text/xml
+		, CGI_MEDIA_TYPE_TEXT_CSV																		// text/csv
+		, CGI_MEDIA_TYPE_TEXT_PLAIN																		// text/plain
+		, CGI_MEDIA_TYPE_IMAGE_PNG																		// image/png
+		, CGI_MEDIA_TYPE_IMAGE_JPEG																		// image/jpeg
+		, CGI_MEDIA_TYPE_IMAGE_GIF																		// image/gif
+		};
+
+	struct SCGIRequestContent {
+		uint32_t																				Length						= {};
+		::gpk::CGI_MEDIA_TYPE																	Type						= {};
+		::gpk::array_pod<char>																	Body						= {};
 	};
 
 	struct SCGIRuntimeValues {
