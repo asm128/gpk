250c624cbaf4f8a97923c55cd3839c59d007fecc
asm128 <pabloarielz@outlook.com>
2019-07-07 14:14:38 -0300

    * Fixed incorrect error reporting.

--- llc/gpk_keyval.cpp
+++ llc/gpk_keyval.cpp
@@ -3,7 +3,7 @@
 
 ::gpk::error_t										gpk::token_split			(char token, const ::gpk::view_const_string& input_string, TKeyValConstString& output_views)	{
 	int32_t													indexToken					= ::gpk::find(token, input_string);
-	gpk_necall(indexToken, "'%c' Token not found.", token);
+	rni_if(errored(indexToken), "'%c' Token not found.", token);
 	output_views.Key									= {input_string.begin(), (uint32_t)indexToken};
 	output_views.Val									= (indexToken + 1 < (int32_t)input_string.size()) ? ::gpk::view_const_string{&input_string[indexToken + 1], input_string.size() - (indexToken + 1)} : ::gpk::view_const_string{};
 	return 0;
