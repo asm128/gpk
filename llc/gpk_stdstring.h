278f6f0a9a0b9ed47636832b4d734cbce60b10ce
asm128 <pabloarielz@outlook.com>
2019-07-13 04:15:11 -0300

    + Added gpk::stoull().

---
+++ llc/gpk_stdstring.h
@@ -0,0 +1,10 @@
+#include "gpk_view_array.h"
+#include "gpk_safe.h"
+
+#ifndef GPK_STDSTRING_H_982137498237
+#define GPK_STDSTRING_H_982137498237
+namespace gpk
+{
+	::gpk::error_t									stoull						(const ::gpk::view_const_string & input, uint64_t * output);
+}
+#endif // GPK_STDSTRING_H_982137498237
