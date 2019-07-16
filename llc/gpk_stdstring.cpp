35b2cca2ed30a844ea32dc06327e0890e5234b18
asm128 <pabloarielz@outlook.com>
2019-07-16 01:05:47 -0300

    + Added ::gpk::tolower().

--- llc/gpk_stdstring.cpp
+++ llc/gpk_stdstring.cpp
@@ -2,7 +2,13 @@
 
 #include <string>
 
-::gpk::error_t									gpk::stoull							(const ::gpk::view_const_string & input, uint64_t * outputNumber)			{
+::gpk::error_t									gpk::tolower						(::gpk::view_char input)											{
+	for(uint32_t iByte = 0, sizeHeader = input.size(); iByte < sizeHeader; ++iByte)
+		input[iByte]						= (byte_t)::tolower(input[iByte]);
+	return 0;
+}
+
+::gpk::error_t									gpk::stoull							(const ::gpk::view_const_string & input, uint64_t * outputNumber)	{
 #if defined(GPK_DISABLE_CPP_EXCEPTIONS)
 	gpk_safe_assign(outputNumber, ::std::stoull({input.begin(), input.size()})); 
 #else
--- llc/gpk_stdstring.h
+++ llc/gpk_stdstring.h
@@ -6,5 +6,6 @@
 namespace gpk
 {
 	::gpk::error_t									stoull						(const ::gpk::view_const_string & input, uint64_t * output);
+	::gpk::error_t									tolower						(::gpk::view_char input);
 }
 #endif // GPK_STDSTRING_H_982137498237
