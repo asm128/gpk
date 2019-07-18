d9609efa413156fb83d4281073bbffb00fca9208
asm128 <pabloarielz@outlook.com>
2019-07-18 10:51:11 -0300

    + Improvements to udp test programs.

--- udp_client/main.cpp
+++ udp_client/main.cpp
@@ -23,5 +23,3 @@ int main() {
 	::gpk::tcpipShutdown();
 	return 0;
 }
-
-
--- udp_server/main.cpp
+++ udp_server/main.cpp
@@ -2,23 +2,21 @@
 
 int main() { 
 	::gpk::tcpipInitialize();
-	sockaddr_in			sa_server			= {};
-	sockaddr_in			sa_client			= {};
-	int					client_length		= sizeof(sa_client);
+	sockaddr_in				sa_server				= {};
 	sa_server.sin_port	= htons(80);
 	::gpk::tcpipAddressToSockaddr({{}, 80}, sa_server);
-
-
 	// 1. Server:
-	SOCKET				handle				= socket(AF_INET, SOCK_DGRAM, 0);
-	char				connectReceived		= 0;
+	SOCKET					handle					= socket(AF_INET, SOCK_DGRAM, 0);
+	char					connectReceived			= 0;
 	::bind(handle, (sockaddr *)&sa_server, sizeof(sockaddr_in));
 	while(true) {
+		sockaddr_in				sa_client				= {};
+		int						client_length			= sizeof(sa_client);
 		gpk_necall(::recvfrom(handle, (char*)&connectReceived, (int)sizeof(char), 0, (sockaddr*)&sa_client, &client_length), "Failed to receive connect request.");
 		info_printf("Received connect request: %c", connectReceived);
 
 		// 3. Server:
-		char				commandToSend		= '2';
+		char					commandToSend			= '2';
 		gpk_necall(::sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr*)&sa_client, sizeof(sockaddr_in)), "Failed to respond.");
 		info_printf("Sending connect response: %c", connectReceived);
 	}
