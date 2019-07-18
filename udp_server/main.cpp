eadf47acbb18d4eaa48e5f9b282d9a755c328669
asm128 <pabloarielz@outlook.com>
2019-07-18 10:54:43 -0300

    + Improvements.

--- udp_server/main.cpp
+++ udp_server/main.cpp
@@ -5,17 +5,17 @@ int main() {
 	sockaddr_in				sa_server				= {};
 	sa_server.sin_port	= htons(80);
 	::gpk::tcpipAddressToSockaddr({{}, 80}, sa_server);
-	// 1. Server:
+
 	SOCKET					handle					= socket(AF_INET, SOCK_DGRAM, 0);
-	char					connectReceived			= 0;
-	::bind(handle, (sockaddr *)&sa_server, sizeof(sockaddr_in));
+	ree_if(INVALID_SOCKET == handle, "Failed to create socket.");
+	gpk_necall(::bind(handle, (sockaddr *)&sa_server, sizeof(sockaddr_in)), "Failed to bind listener to address");
 	while(true) {
 		sockaddr_in				sa_client				= {};
 		int						client_length			= sizeof(sa_client);
+		char					connectReceived			= 0;
 		gpk_necall(::recvfrom(handle, (char*)&connectReceived, (int)sizeof(char), 0, (sockaddr*)&sa_client, &client_length), "Failed to receive connect request.");
 		info_printf("Received connect request: %c", connectReceived);
 
-		// 3. Server:
 		char					commandToSend			= '2';
 		gpk_necall(::sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr*)&sa_client, sizeof(sockaddr_in)), "Failed to respond.");
 		info_printf("Sending connect response: %c", connectReceived);
