e09b0182681d22c43bde737991c48bd6e220f748
asm128 <pabloarielz@outlook.com>
2019-07-19 04:54:32 -0300

    + Attempt to UDP punch with non-blocking client socket, so the client never stops throttling to every port until connection is made.

--- udp_client/main.cpp
+++ udp_client/main.cpp
@@ -14,27 +14,46 @@ int main() {
 #endif
 		::gpk::SIPv4		addrRemote			= {};
 		SOCKET				handle				= socket(AF_INET, SOCK_DGRAM, 0);
+#if defined GPK_WINDOWS
+		u_long				iMode				= 1;
+		int					iResult				= ioctlsocket(handle, FIONBIO, &iMode);
+		gpk_necall(iResult, "could not set file flags");
+#else
+		int					flags				= fcntl(socket_fd, F_GETFL);
+		gpk_necall(flags, "could not get file flags");
+		gpk_necall(fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK), "could not set file flags");
+#endif
 		ree_if(INVALID_SOCKET == handle, "Failed to create socket.");
 		sockaddr_in			sa_client			= {AF_INET};
 		gpk_necall(::bind(handle, (sockaddr *)&sa_client, sizeof(sockaddr_in)), "Failed to bind listener to address");
 		char				commandToSend		= '1';
 		int					sa_length			= sizeof(sa_server);
-		gpk_necall(sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr *)&sa_server, sa_length), "Failed to send connect request to server.");
-		{
-			sockaddr_in			sa_battery			= sa_server;
-			for(uint32_t j=10; j < 10; ++j) {
-			for(uint32_t i=16*1024; i < 64*1024; ++i) {
-				sa_battery.sin_port	= htons((u_short)i);
-				gpk_necall(sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr *)&sa_battery, sa_length), "Failed to send connect request to server.");
-				//::gpk::sleep(1);
-			}}
-		}
+		gpk_necall(::sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr *)&sa_server, sa_length), "Failed to send connect request to server.");
 		::gpk::tcpipAddressFromSockaddr(sa_server, addrRemote);
 		info_printf("Sent connect request to server: %c to %u.%u.%u.%u:%u", commandToSend, GPK_IPV4_EXPAND(addrRemote));
 
 		char				connectAcknowledge	= 0;
 		sa_server.sin_port	= 0;
-		gpk_necall(recvfrom(handle, (char *)&connectAcknowledge, (int)sizeof(char), 0, (sockaddr *)&sa_server, &sa_length), "Failed to receive response from server");
+		while(true) {
+			connectAcknowledge	= 0;
+			ree_if(-1 == ::recvfrom(handle, (char *)&connectAcknowledge, (int)1, MSG_PEEK, (sockaddr *)&sa_server, &sa_length) && WSAGetLastError() != WSAEMSGSIZE && WSAGetLastError() != WSAEWOULDBLOCK, "Failed to receive response from server");
+
+			if('2' == connectAcknowledge) {
+				info_printf("Connected!");
+				break;
+			}
+			else {
+				::gpk::_gpk_print_system_errors("1", 1);
+				sockaddr_in			sa_battery			= sa_server;
+				for(uint32_t j=10; j < 10; ++j) {
+				for(uint32_t i=16*1024; i < 64*1024; ++i) {
+					sa_battery.sin_port	= htons((u_short)i);
+					gpk_necall(::sendto(handle, (const char*)&commandToSend, (int)sizeof(char), 0, (sockaddr *)&sa_battery, sa_length), "Failed to send connect request to server.");
+					//::gpk::sleep(1);
+			}}
+		}
+
+		}
 		addrRemote			= {};
 		::gpk::tcpipAddressFromSockaddr(sa_server, addrRemote);
 		info_printf("Received connect response from server: %c from %u.%u.%u.%u:%u.", connectAcknowledge, GPK_IPV4_EXPAND(addrRemote));
