655629a1e4a518ce73021e6ec23c6df3d7960856
asm128 <pabloarielz@outlook.com>
2019-08-31 02:13:04 -0300

    + Added function compatible with JS counterpart.

--- llc/gpk_noise.h
+++ llc/gpk_noise.h
@@ -8,9 +8,10 @@ namespace gpk
 
 	static constexpr	const uint64_t			NOISE_SEED			= 16381; // 525253
 
-						uint64_t				noise1DBase			(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
-						double					noise1D				(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
-						double					noiseNormal1D		(uint64_t x, uint64_t noiseSeed = 16381)																noexcept;
+						uint32_t				noise1DBase32		(uint32_t x, uint32_t noiseSeed = 15661)	noexcept;
+						uint64_t				noise1DBase			(uint64_t x, uint64_t noiseSeed = 16381)	noexcept;
+						double					noise1D				(uint64_t x, uint64_t noiseSeed = 16381)	noexcept;
+						double					noiseNormal1D		(uint64_t x, uint64_t noiseSeed = 16381)	noexcept;
 						double					noise2D				(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
 						double					noise3D				(uint32_t x, uint32_t y, uint32_t z	, uint32_t nWidth, uint32_t nHeight	, uint64_t noiseSeed = 16381)	noexcept;
 						double					noiseNormal2D		(uint32_t x, uint32_t y				, uint32_t nWidth					, uint64_t noiseSeed = 16381)	noexcept;
@@ -22,9 +23,9 @@ namespace gpk
 							uint64_t				Position			= 0;
 							uint64_t				Value				= 0;
 
-		inline				uint64_t				Next				()																										noexcept	{ return Value = ::gpk::noise1DBase(++Position, Seed); }
-							void					Reset				()																										noexcept	{ Reset(Seed); }
-							void					Reset				(uint64_t seed)																							noexcept	{
+		inline				uint64_t				Next				()				noexcept	{ return Value = ::gpk::noise1DBase(++Position, Seed); }
+							void					Reset				()				noexcept	{ Reset(Seed); }
+							void					Reset				(uint64_t seed)	noexcept	{
 			Seed										= seed;
 			Position									= 0;
 			Value										= 0;
