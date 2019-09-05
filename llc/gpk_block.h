8677f68de97f04ee67f94907a1c9f81950aa4ebe
asm128 <pabloarielz@outlook.com>
2019-09-05 01:26:10 -0300

    + Added ::gpk::SMapBlock<>.

---
+++ llc/gpk_block.h
@@ -0,0 +1,14 @@
+#ifndef GPK_BLOCK_H_9823749283749823
+#define GPK_BLOCK_H_9823749283749823
+
+
+namespace gpk
+{
+	template<typename _tBlock>
+	struct SMapBlock {
+		::gpk::array_pod<uint64_t>					Offset;
+		::gpk::array_obj<_tBlock>					Block;
+	};
+} // namespace
+
+#endif // GPK_BLOCK_H_9823749283749823
