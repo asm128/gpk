2c20c8a89ce2c7e1500c7a78b15f409f41b5d704
asm128 <pabloarielz@outlook.com>
2020-10-08 18:17:02 -0300

    + Added missing const.

--- llc/gpk_rigidbody.cpp
+++ llc/gpk_rigidbody.cpp
@@ -1,15 +1,15 @@
 #include "gpk_rigidbody.h"
 
 void												gpk::transformInertiaTensor			(::gpk::SMatrix3<float> & iitWorld, const ::gpk::SMatrix3<float> &iitBody, const ::gpk::SMatrix4<float> &rotmat)		{
-	float													t4									= rotmat._11*iitBody._11 + rotmat._21*iitBody._12 + rotmat._31*iitBody._13;
-	float													t9									= rotmat._11*iitBody._21 + rotmat._21*iitBody._22 + rotmat._31*iitBody._23;
-	float													t14									= rotmat._11*iitBody._31 + rotmat._21*iitBody._32 + rotmat._31*iitBody._33;
-	float													t28									= rotmat._12*iitBody._11 + rotmat._22*iitBody._12 + rotmat._32*iitBody._13;
-	float													t33									= rotmat._12*iitBody._21 + rotmat._22*iitBody._22 + rotmat._32*iitBody._23;
-	float													t38									= rotmat._12*iitBody._31 + rotmat._22*iitBody._32 + rotmat._32*iitBody._33;
-	float													t52									= rotmat._13*iitBody._11 + rotmat._23*iitBody._12 + rotmat._33*iitBody._13;
-	float													t57									= rotmat._13*iitBody._21 + rotmat._23*iitBody._22 + rotmat._33*iitBody._23;
-	float													t62									= rotmat._13*iitBody._31 + rotmat._23*iitBody._32 + rotmat._33*iitBody._33;
+	const float												t4									= rotmat._11*iitBody._11 + rotmat._21*iitBody._12 + rotmat._31*iitBody._13;
+	const float												t9									= rotmat._11*iitBody._21 + rotmat._21*iitBody._22 + rotmat._31*iitBody._23;
+	const float												t14									= rotmat._11*iitBody._31 + rotmat._21*iitBody._32 + rotmat._31*iitBody._33;
+	const float												t28									= rotmat._12*iitBody._11 + rotmat._22*iitBody._12 + rotmat._32*iitBody._13;
+	const float												t33									= rotmat._12*iitBody._21 + rotmat._22*iitBody._22 + rotmat._32*iitBody._23;
+	const float												t38									= rotmat._12*iitBody._31 + rotmat._22*iitBody._32 + rotmat._32*iitBody._33;
+	const float												t52									= rotmat._13*iitBody._11 + rotmat._23*iitBody._12 + rotmat._33*iitBody._13;
+	const float												t57									= rotmat._13*iitBody._21 + rotmat._23*iitBody._22 + rotmat._33*iitBody._23;
+	const float												t62									= rotmat._13*iitBody._31 + rotmat._23*iitBody._32 + rotmat._33*iitBody._33;
 
 	iitWorld._11										= t4 *rotmat._11 + t9 *rotmat._21 + t14*rotmat._31;
 	iitWorld._21										= t4 *rotmat._12 + t9 *rotmat._22 + t14*rotmat._32;
@@ -22,14 +22,14 @@ void												gpk::transformInertiaTensor			(::gpk::SMatrix3<float> & iitWorld
 	iitWorld._33										= t52*rotmat._13 + t57*rotmat._23 + t62*rotmat._33;
 }
 
-void												gpk::updateTransform					(::gpk::STransform3 & bodyTransform, ::gpk::SMatrix4<float> & transformLocal)	{
+void												gpk::updateTransform				(::gpk::STransform3 & bodyTransform, ::gpk::SMatrix4<float> & transformLocal)	{
 	//transformLocal.Scale( bodyTransform.Scale, true );
 	bodyTransform.Orientation.Normalize();
 	transformLocal.SetOrientation( bodyTransform.Orientation );
 	transformLocal.SetTranslation( bodyTransform.Position, false );
 }
 
-int32_t												gpk::integrateForces					(double duration, ::gpk::SRigidBodyFrame& bodyFrame, ::gpk::SForce3 & bodyForce, const ::gpk::SMass3 & bodyMass) {
+int32_t												gpk::integrateForces				(double duration, ::gpk::SRigidBodyFrame& bodyFrame, ::gpk::SForce3 & bodyForce, const ::gpk::SMass3 & bodyMass) {
 	// -- Calculate linear acceleration from force inputs.
 	bodyFrame.LastFrameAcceleration						= bodyForce.Acceleration;
 	bodyFrame.LastFrameAcceleration.AddScaled(bodyFrame.AccumulatedForce, bodyMass.InverseMass);
@@ -37,7 +37,7 @@ int32_t												gpk::integrateForces					(double duration, ::gpk::SRigidBodyF
 	bodyForce.Velocity.AddScaled(bodyFrame.LastFrameAcceleration, (float)duration);	// Update linear velocity from both acceleration and impulse.
 
 	// -- Calculate angular acceleration from torque inputs.
-	::gpk::SCoord3<float>									angularAcceleration						= bodyFrame.InverseInertiaTensorWorld.Transform(bodyFrame.AccumulatedTorque);
+	const ::gpk::SCoord3<float>								angularAcceleration					= bodyFrame.InverseInertiaTensorWorld.Transform(bodyFrame.AccumulatedTorque);
 	bodyForce.Rotation.AddScaled(angularAcceleration, duration);	// Update angular velocity from both acceleration and impulse.
 
 	// Impose drag.
@@ -67,17 +67,17 @@ int													gpk::createOrbiter
 	, double				orbital_inclination
 	, double				distance_scale
 	) {
-	const int32_t											indexFirstBody					= bodies.Spawn(2);
+	const int32_t											indexFirstBody						= bodies.Spawn(2);
 	{
-		::gpk::STransform3										& orbitTransform				= bodies.Transforms	[indexFirstBody]		= {};
-		::gpk::SForce3											& orbitForces					= bodies.Forces		[indexFirstBody]		= {};
+		::gpk::STransform3										& orbitTransform					= bodies.Transforms	[indexFirstBody]		= {};
+		::gpk::SForce3											& orbitForces						= bodies.Forces		[indexFirstBody]		= {};
 		orbitForces.Rotation								= {0.0f, (float)(::gpk::math_2pi / orbital_period), 0.0f};			// Set the orbital rotation velocity IN EARTH DAYS
 		orbitTransform.Orientation.MakeFromEulerTaitBryan( (float)(::gpk::math_2pi / 360.0 * orbital_inclination), 0.0f, 0.0f );	// Calculate the orbital tilt IN RADIANS
 	}
 	{
-		::gpk::SMass3											& planetMass					= bodies.Masses		[indexFirstBody + 1]	= {};
-		::gpk::STransform3										& planetTransform				= bodies.Transforms	[indexFirstBody + 1]	= {};
-		::gpk::SForce3											& planetForces					= bodies.Forces		[indexFirstBody + 1]	= {};
+		::gpk::SMass3											& planetMass						= bodies.Masses		[indexFirstBody + 1]	= {};
+		::gpk::STransform3										& planetTransform					= bodies.Transforms	[indexFirstBody + 1]	= {};
+		::gpk::SForce3											& planetForces						= bodies.Forces		[indexFirstBody + 1]	= {};
 		planetMass.InverseMass								= float(1.0 / mass);
 		planetTransform.Position.x							= float(distance_scale * distance);
 
