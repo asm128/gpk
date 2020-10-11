e0951b3a7877e92117eaf4cb82e0c22810bc0e25
asm128 <pabloarielz@outlook.com>
2020-10-11 11:04:25 -0300

    + Replaced parameter types in order to avoid problems by the unwary.

--- llc/gpk_keyval.cpp
+++ llc/gpk_keyval.cpp
@@ -28,7 +28,7 @@
 	return sprintfable;
 }
 
-::gpk::error_t							gpk::token_split					(char token, const ::gpk::view_const_char& input_string, TKeyValConstString& output_views)	{
+::gpk::error_t							gpk::token_split					(char token, const ::gpk::view_const_string& input_string, TKeyValConstString& output_views)	{
 	int32_t										indexToken							= ::gpk::find(token, input_string);
 	rvi_if(-1, errored(indexToken), "'%c' Token not found.", token);
 	output_views.Key						= {input_string.begin(), (uint32_t)indexToken};
@@ -41,13 +41,13 @@
 	return 0;
 }
 
-::gpk::error_t							gpk::find							(const ::gpk::view_const_char & keyToFind, const ::gpk::view_array<const ::gpk::TKeyValConstString> & keyvals, ::gpk::view_const_char& out_val)		{
+::gpk::error_t							gpk::find							(const ::gpk::view_const_string & keyToFind, const ::gpk::view_array<const ::gpk::TKeyValConstString> & keyvals, ::gpk::view_const_char& out_val)		{
 	::gpk::error_t								index								= ::gpk::find(keyToFind, keyvals);
 	out_val									= (-1 == index) ? ::gpk::view_const_string{} : keyvals[index].Val;
 	return index;
 }
 
-::gpk::error_t							gpk::keyvalNumeric					(const ::gpk::view_const_char & key, const ::gpk::view_array<const ::gpk::TKeyValConstString> keyVals, uint64_t * outputNumber)	{
+::gpk::error_t							gpk::keyvalNumeric					(const ::gpk::view_const_string & key, const ::gpk::view_array<const ::gpk::TKeyValConstString> keyVals, uint64_t * outputNumber)	{
 	ree_if(0 == outputNumber, "%s", "Output number cannot point to a null address.");
 	::gpk::error_t								indexKey							= ::gpk::find(key, keyVals);
 	if(-1 != indexKey) {
