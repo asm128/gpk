4e9fb45637bfd9c146b144770d4dbf35539c6794
asm128 <pabloarielz@outlook.com>
2021-03-30 15:16:44 -0300

    + Moved gpk_display.* to llc.

---
+++ llc/gpk_display.h
@@ -0,0 +1,58 @@
+#include "gpk_coord.h"
+#include "gpk_color.h"
+#include "gpk_view_grid.h"
+
+#if defined(GPK_WINDOWS)
+#	include <Windows.h>
+#endif
+
+#ifndef GPK_DISPLAY_H_293874928374
+#define GPK_DISPLAY_H_293874928374
+
+namespace gpk
+{
+	struct SDisplayPlatformDetail {
+#if defined(GPK_WINDOWS)
+							::HWND								WindowHandle								= {};
+							::WNDCLASSEX						WindowClass									= {};
+
+		static constexpr	const TCHAR							WindowClassName	[256]						= TEXT("SUGAR_WINDOW");
+#endif
+	};
+
+#if defined(GPK_WINDOWS)
+	static inline		void								initWndClass								(::HINSTANCE hInstance, const TCHAR* className, ::WNDCLASSEX& wndClassToInit, WNDPROC wndProc)	{
+		wndClassToInit											= {sizeof(::WNDCLASSEX),};
+		wndClassToInit.lpfnWndProc								= wndProc;
+		wndClassToInit.hInstance								= hInstance;
+		wndClassToInit.hCursor									= LoadCursor(NULL, IDC_ARROW);
+		wndClassToInit.hbrBackground							= (::HBRUSH)(COLOR_3DFACE + 1);
+		wndClassToInit.lpszClassName							= className;
+	}
+#endif
+
+	struct SDisplay {
+							::gpk::SDisplayPlatformDetail		PlatformDetail								= {};
+							::gpk::SCoord2<uint32_t>			PreviousSize								= {};
+							::gpk::SCoord2<uint32_t>			Size										= {320, 200};
+							bool								Resized										: 1;
+							bool								Repaint										: 1;
+							bool								NoDraw										: 1;
+							bool								MinOrMaxed									: 1;
+							bool								Closed										: 1;
+
+																SDisplay									()		noexcept
+			: Resized		(true)
+			, Repaint		(true)
+			, NoDraw		(false)
+			, MinOrMaxed	(false)
+			, Closed		(false)
+		{}
+	};
+
+	::gpk::error_t											displayPresentTarget						(::gpk::SDisplay& displayInstance, const ::gpk::view_grid<::gpk::SColorBGRA>& targetToPresent);
+	::gpk::error_t											displayUpdate								(::gpk::SDisplay& applicationInstance);
+	::gpk::error_t											displayUpdateTick							(::gpk::SDisplay& displayInstance);
+} // namespace
+
+#endif // GPK_DISPLAY_H_293874928374
