4aaade0b4ed2350499db3f12740fabe23a953582
asm128 <pabloarielz@outlook.com>
2021-05-07 00:53:28 -0300

    * Fix.

--- llc/gpk_stl.h
+++ llc/gpk_stl.h
@@ -11,12 +11,12 @@ namespace gpk {
 		::gpk::STriangle3<float>				Triangle	= {};
 		uint16_t								Attribute	= {};
 	};
+#pragma pack(pop)
 	struct SSTLFile {
 		::gpk::array_pod<byte_t>				Raw			= {};
 		::gpk::view_byte						Header		= {};
 		::gpk::view_array<::gpk::SSTLTriangle>	Triangles	= {};
 	};
-#pragma pack(pop)
 
 	::gpk::error_t			stlFileLoad			(::gpk::view_const_char filename, ::gpk::SSTLFile& file);
 	::gpk::error_t			stlFileLoad			(::gpk::view_byte fileInMemory, ::gpk::view_byte & out_Header, ::gpk::view_array<::gpk::SSTLTriangle>	& out_Triangles);
