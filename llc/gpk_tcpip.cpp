5d0fad6279f850863aa772986f03ab87ec341479
asm128 <pabloarielz@outlook.com>
2023-06-28 04:12:32 -0300

    Got the connection working and idle.

--- llc/gpk_tcpip.cpp
+++ llc/gpk_tcpip.cpp
@@ -187,11 +187,11 @@
 	return iAddress;
 }
 
-::gpk::error_t			gpk::tcpipAddress					(const ::gpk::view<const char>& strRemoteIP, const ::gpk::view<const char>& strRemotePort, ::gpk::SIPv4 & remoteIP) {
-	::gpk::parseIntegerDecimal(strRemotePort, (remoteIP.Port = 0));
+::gpk::error_t			gpk::tcpipAddress		(const ::gpk::view<const char>& strRemoteIP, const ::gpk::view<const char>& strRemotePort, ::gpk::SIPv4 & remoteIP) {
+	gpk_necs(::gpk::parseIntegerDecimal(strRemotePort, (remoteIP.Port = 0)));
 	if(strRemoteIP.size()) {
-		uint32_t											iOffset						= 0;
-		uint32_t											iEnd						= 0;
+		uint32_t					iOffset					= 0;
+		uint32_t					iEnd					= 0;
 		for(uint32_t iVal = 0; iVal < 4; ++iVal) {
 			while(iEnd < strRemoteIP.size()) {
 				char curChar = strRemoteIP[iEnd];
@@ -204,13 +204,12 @@
 				++iEnd;
 			}
 			::gpk::parseIntegerDecimal({&strRemoteIP[iOffset], iEnd - iOffset}, (remoteIP.IP[iVal] = 0));
-			iOffset											= iEnd + 1;
-			iEnd											= iOffset;
+			iOffset					= iEnd + 1;
+			iEnd					= iOffset;
 		}
 		if(0 == strRemotePort.size() && iOffset != strRemoteIP.size()) {
-			if(strRemotePort.size()) {
-				::gpk::parseIntegerDecimal({&strRemoteIP[iOffset], strRemoteIP.size() - iOffset}, remoteIP.Port);
-			}
+			if(strRemotePort.size())
+				gpk_necs(::gpk::parseIntegerDecimal({&strRemoteIP[iOffset], strRemoteIP.size() - iOffset}, remoteIP.Port));
 		}
 	}
 	return 0;
