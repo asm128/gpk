952f3ccf53c47747bf60da433f0a20dbc6762520
asm128 <pabloarielz@outlook.com>
2023-07-01 11:40:59 -0300

    * Fixes.

--- gpk/gpk_udp_server_app.cpp
+++ gpk/gpk_udp_server_app.cpp
@@ -66,14 +66,18 @@ static	::gpk::error_t	updateGUI		(::gpk::SServer & server, ::gpk::SGUI & gui)		{
 	es_if_failed(::gpk::serverPayloadCollect(server.UDP, server.QueueReceived));
 
 	server.UDP.Clients.enumerate([&server](uint32_t & iClient, ::gpk::pobj<::gpk::SUDPConnection> & client){
-		server.QueueToSend[iClient]->for_each([&client](::gpk::pau8 & payload) {
-			if(payload && payload->size())
-				gpk_necs(::gpk::connectionPushData(*client, client->Queue, *payload));
+		if(server.QueueToSend.size() <= iClient)
+			iClient					= server.UDP.Clients.size();
+		else {
+			server.QueueToSend[iClient]->for_each([&client](::gpk::pau8 & payload) {
+				if(payload && payload->size())
+					gpk_necs(::gpk::connectionPushData(*client, client->Queue, *payload));
 
-			payload.clear();
- 			return 0;
-		});
-		server.QueueToSend[iClient].clear();
+				payload.clear();
+ 				return 0;
+			});
+			server.QueueToSend[iClient].clear();
+		}
 	});
 	return ::updateGUI(server, gui);
 }
