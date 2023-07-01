0c015643de25a7fa7ec5fcb05df4df33769bcbf9
asm128 <pabloarielz@outlook.com>
2023-07-01 10:18:46 -0300

    + Added functions to calculate the sun position and daylight level.

---
+++ llc/gpk_sun.cpp
@@ -0,0 +1,19 @@
+#include "gpk_sun.h"
+#include <ctime>
+
+double					gpk::calcSunlightFactor(double daylightRatioExtra, uint32_t daylightOffsetMinutes)	{ 
+	const double				sunAngleFactor			= ::gpk::calcSunPosition().Dot(::gpk::n3f32{0, 1}.RotateZ(::gpk::SCALE_DAY_MINUTES_TO_RADIANS * daylightOffsetMinutes));
+	return ::gpk::clamp(sunAngleFactor + daylightRatioExtra, 0.0, 1.0);
+}
+
+::gpk::n3f32			gpk::calcSunPosition	()	{ 
+	const time_t				now						= time(0);
+	tm							ltm						= {};
+	localtime_s(&ltm, &now);
+
+	const uint32_t				hourInSeconds			= ltm.tm_hour * 60 * 60;
+	const uint32_t				minsInSeconds			= ltm.tm_min * 60;
+	const uint32_t				daySeconds				= hourInSeconds + minsInSeconds + ltm.tm_sec;
+
+	return ::gpk::n3f32{0, -1}.RotateZ(::gpk::SCALE_DAY_SECONDS_TO_RADIANS * daySeconds);
+}
---
+++ llc/gpk_sun.h
@@ -0,0 +1,16 @@
+#include "gpk_n3.h"
+
+#ifndef GPK_SUN_H_23627
+#define GPK_SUN_H_23627
+
+namespace gpk
+{
+	stacxpr	double	SCALE_DAY_SECONDS				= 1.0 / 86400;
+	stacxpr	double	SCALE_DAY_SECONDS_TO_RADIANS	= ::gpk::math_2pi * SCALE_DAY_SECONDS;
+	stacxpr	double	SCALE_DAY_MINUTES_TO_RADIANS	= SCALE_DAY_SECONDS_TO_RADIANS * 60;
+
+	::gpk::n3f32	calcSunPosition					();
+	double			calcSunlightFactor				(double daylightRatioExtra, uint32_t daylightOffsetMinutes);
+}
+
+#endif // GPK_SUN_H_23627
