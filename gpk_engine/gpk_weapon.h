4e92612f5810bd77a61a17eabf41b3708f819e9f
asm128 <pabloarielz@outlook.com>
2023-07-15 03:49:55 -0300

    + Improved weapons.

--- gpk_engine/gpk_weapon.h
+++ gpk_engine/gpk_weapon.h
@@ -4,13 +4,27 @@
 #include "gpk_apod_serialize.h"
 #include "gpk_gauge.h"
 
-#ifndef GPK_GALAXY_HELL_WEAPON_H
-#define GPK_GALAXY_HELL_WEAPON_H
+#ifndef GPK_WEAPON_H
+#define GPK_WEAPON_H
 
 namespace gpk 
 {
+
+	typedef	::gpk::aobj<::gpk::an3char	>	aan3c	, aan3char		;
+	typedef	::gpk::aobj<::gpk::an3uchar	>	aan3uc	, aan3uchar		;
+	typedef	::gpk::aobj<::gpk::an3f32	>	aan3f32	;
+	typedef	::gpk::aobj<::gpk::an3f64	>	aan3f64	;
+	typedef	::gpk::aobj<::gpk::an3u8	>	aan3u8	;
+	typedef	::gpk::aobj<::gpk::an3u16	>	aan3u16	;
+	typedef	::gpk::aobj<::gpk::an3u32	>	aan3u32	;
+	typedef	::gpk::aobj<::gpk::an3u64	>	aan3u64	;
+	typedef	::gpk::aobj<::gpk::an3i8	>	aan3i8	;
+	typedef	::gpk::aobj<::gpk::an3i16	>	aan3i16	;
+	typedef	::gpk::aobj<::gpk::an3i32	>	aan3i32	;
+	typedef	::gpk::aobj<::gpk::an3i64	>	aan3i64	;
+
 	struct SShots	{
-		::gpk::aapod<::gpk::n3f32>	DistanceToTargets	= {};
+		::gpk::aan3f32				DistanceToTargets	= {};
 		::gpk::af32					Lifetime			= {};
 		::gpk::af32					Brightness			= {};
 		::gpk::an3f32				PositionDraw		= {};
@@ -35,13 +49,13 @@ namespace gpk
 			return Particles.Create(position, direction, speed);
 		}
 
-		int							SpawnForcedDirected	(double stabilityFactor, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float speedDebris, float brightness, float lifetime)	{
+		int							SpawnForcedDirected	(double stabilityFactor, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float speed, float brightness, float lifetime)	{
 			stacxpr	double					randUnit			= ::gpk::math_2pi / RAND_MAX;
-			::gpk::n3f32						finalDirection		= {0, 1, 0};
+			::gpk::n3f32					finalDirection		= {0, 1, 0};
 			finalDirection.RotateX(rand() * randUnit);
 			finalDirection.RotateY(rand() * randUnit);
 			finalDirection.Normalize();
-			return SpawnForced(position, ::gpk::interpolate_linear(finalDirection, direction, stabilityFactor), speedDebris, brightness, lifetime);
+			return SpawnForced(position, ::gpk::interpolate_linear(finalDirection, direction, stabilityFactor), speed, brightness, lifetime);
 		}
 
 		::gpk::error_t				Update				(float secondsLastFrame)	{
@@ -86,33 +100,74 @@ namespace gpk
 	};
 
 #pragma pack(push, 1)
-	GDEFINE_FLAG_TYPE(AMMO_CLASS, int32_t);
+
+	struct SWeaponTrigger {
+		gaugemaxf32		Delay				= {};	// Trigger delay
+		gaugemaxf32		Overheat			= {};	// Cooldown/Reload time in seconds
+		float			OverheatPerShot		= {};	// 
+		bool			CoolingDown			= {};	// Reloading
+
+		inlcxpr	float	Busy				()	const			noexcept	{ 
+			return CoolingDown					? ::gpk::max(Delay.Limit - Delay.Value, Overheat.Value)
+				: (Delay.Value < Delay.Limit)	? Delay.Limit - Delay.Value
+				: 0
+				; 
+		}
+		float			Pull				()					noexcept	{ 
+			const float			busy				= Busy();
+			if(busy)
+				return busy;
+
+			Delay.Value		= 0;
+			Overheat.Value	+= OverheatPerShot;
+			if(Overheat.Value >= Overheat.Limit)
+				CoolingDown		= true;
+			return 0;
+		}
+		float			Update				(double seconds)	noexcept		{
+			Delay.Value		+= (float)seconds;
+			if(0 >= Overheat.Value && CoolingDown)
+				CoolingDown		= Overheat.Value	= 0;
+			else
+				Overheat.Value	-= (float)seconds;
+
+			return Busy();
+		}
+
+	};
+
+	GDEFINE_FLAG_TYPE(WEAPON_LOAD, uint32_t);
 	// these are the ammo classes that I can think of right now
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, None			,	0x00000UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Arrow		,	0x00001UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Bullet		,	0x00002UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Grenade		,	0x00004UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Rocket		,	0x00008UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, None		,	0);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Arrow		,	0x1UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Bullet		,	0x2UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Grenade		,	0x4UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Rocket		,	0x8UL);	
 	//
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Ray			,	0x00010UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Dart			,	0x00020UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Flame		,	0x00040UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Fluid		,	0x00080UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Ray			,	0x10UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Dart		,	0x20UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Flame		,	0x40UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Fluid		,	0x80UL);	
 	//
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Shot			,	0x00100UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Knife		,	0x00200UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Rock			,	0x00400UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Shrapnel		,	0x00800UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Shot		,	0x100UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Knife		,	0x200UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Rock		,	0x400UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Shrapnel	,	0x800UL);	
 	//
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Boomerang	,	0x01000UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Lightning	,	0x02000UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Hook			,	0x04000UL);	
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Wave			,	0x08000UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Boomerang	,	0x1000UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Lightning	,	0x2000UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Hook		,	0x4000UL);	
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Wave		,	0x8000UL);	
 	//
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Blast		,	0x010000UL);
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Singularity	,	0x020000UL);
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Temporized	,	0x100000UL);
-	GDEFINE_FLAG_VALUE(AMMO_CLASS, Detonable	,	0x200000UL);
+	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Missile		,	0x10000UL);
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Blast		,	0x20000UL);
+	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Cannonball	,	0x40000UL);
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Flare		,	0x80000UL);
+
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Singularity	,	0x100000UL);
+
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Temporized	,	0x10000000UL);
+	GDEFINE_FLAG_VALUE(WEAPON_LOAD, Detonable	,	0x20000000UL);
 
 	GDEFINE_ENUM_TYPE(WEAPON_ACTION, uint16_t);
 	GDEFINE_ENUM_VALUE(WEAPON_ACTION, Shoot		, 0x0);
@@ -141,98 +196,38 @@ namespace gpk
 	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Missile		, 3);
 	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Torpedo		, 4);
 	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Shotgun		, 5);
-	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Mothership	, 6);
-	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Shield		, 7);
+	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Battery		, 6);
+	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Mothership	, 7);
+	GDEFINE_ENUM_VALUE(WEAPON_TYPE, Shield		, 8);
 
-	GDEFINE_ENUM_TYPE (WEAPON_LOAD, uint8_t);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Bullet		, 0);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Shot		, 1);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Ray			, 2);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Cannonball	, 3);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Rocket		, 4);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Missile		, 5);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Wave		, 6);
-	GDEFINE_ENUM_VALUE(WEAPON_LOAD, Flare		, 7);
-
-	GDEFINE_ENUM_TYPE(WEAPON_EFFECT, uint8_t);
-	GDEFINE_ENUM_VALUE(WEAPON_EFFECT, Pierce	, 0);
-
-	// One per value combination
-	struct SWeaponLoadDesc {
-		WEAPON_LOAD		Type				= {};//= WEAPON_LOAD_Bullet;
-		DAMAGE_TYPE		DamageType			= {};//= WEAPON_DAMAGE_Pierce;
-		uint8_t			ParticleCount		= {};//= 1;
-		float			Delay				= {};//= .1;
-		int32_t			Damage				= {};//= 1;
-		float			Lifetime			= {};//= 0;
-		float			Speed				= {};//= 150;
-		float			Weight				= {};//= 150;
-		//uint32_t		Color				= {};
-	};
-	
-	// One per value combinatiom
-	struct SWeaponType {
-		WEAPON_TYPE		Type				= {};//= WEAPON_TYPE_Gun;
-		DAMAGE_TYPE		DamageType			= {};//= WEAPON_DAMAGE_Pierce;
-		int32_t			DamageMultiplier	= {};//= 1;
-		float			Cooldown			= {};//= 1;
-		float			Stability			= {};//= 1.0;
-		float			ShotLifetime		= {};//= 0;
-		float			SpeedMultiplier		= {};//= 150;
-	};
-
-	struct SWeaponLoadState {
-		gaugemaxf32		Delay				= {};	//= .1;
-		gaugemaxf32		Overheat			= {};	//= 1; aka Reload
-		bool			CoolingDown			= {};	//= false;
+	struct SWeaponShot {
+		WEAPON_LOAD		Type				= {};	//= WEAPON_LOAD_Bullet;
+		DAMAGE_TYPE		DamageType			= {};	//= WEAPON_DAMAGE_Pierce;
+		uint16_t		ParticleCount		= 1;	//= 1;
+		int32_t			Damage				= 1;	//= 1;
+		float			Stability			= 1;	//= 1.0;
+		float			Lifetime			= .5f;	//= 0;
+		float			Speed				= {};	//= 150;
 	};
 
 	// One per orbiter
 	struct SWeapon {
-		WEAPON_TYPE		Type				= {};	//= WEAPON_TYPE_Gun;
-		WEAPON_LOAD		Load				= {};	//= WEAPON_LOAD_Bullet;
-		DAMAGE_TYPE		DamageType			= {};	//= WEAPON_DAMAGE_Pierce;
-		uint8_t			ParticleCount		= {};	//= 1;
-		gaugemaxf32		Delay				= {};	// Time in seconds between a shot and the next
-		gaugemaxf32		Overheat			= {};	// Cooldown/Reload time in seconds
-		float			OverheatPerShot		= {};	// 
-		float			Stability			= {};	//= 1.0;
-		float			Speed				= {};	//= 150;
-		int32_t			Damage				= {};	//= 1;
-		float			ShotLifetime		= {};	//= 0;
-		bool			CoolingDown			= {};	//= false;
+		WEAPON_TYPE		Type				= {};
+		SWeaponTrigger	Trigger				= {};
+		SWeaponShot		Shot				= {};
 
-		int				Shoot				(::gpk::SShots & shots, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float speed, float brightness, float lifetime)	{
-			if(Delay.Value < Delay.Limit)
+		int				Shoot				(::gpk::SShots & shots, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float brightness)	{
+			if(Trigger.Pull())
 				return 0;
 
-			Delay.Value		= 0;
-			Overheat.Value	+= OverheatPerShot;
-			return shots.SpawnForced(position, direction, speed, brightness, lifetime);
-		}
+			if(0 == Shot.ParticleCount) 
+				return shots.SpawnForcedDirected(Shot.Stability, position, direction, Shot.Speed, brightness, Shot.Lifetime);
 
-		int				ShootDirected		(::gpk::SShots & shots, double stabilityFactor, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float speedDebris, float brightness, float lifetime)	{
-			if(Delay.Value < Delay.Limit)
-				return 0;
+			const int32_t		indexFirst			= shots.SpawnForcedDirected(Shot.Stability, position, direction, Shot.Speed, brightness, Shot.Lifetime);
+			const uint32_t		stop				= uint32_t(Shot.ParticleCount - 1);
+			for(uint32_t iDebris = 0; iDebris < stop; ++iDebris)
+				shots.SpawnForcedDirected(Shot.Stability, position, direction, Shot.Speed, brightness, Shot.Lifetime);
 
-			Delay.Value		= 0;
-			Overheat.Value	+= OverheatPerShot;
-			return shots.SpawnForcedDirected(Stability * stabilityFactor, position, direction, speedDebris, brightness, lifetime);
-		}
-
-		int				ShootDirected		(::gpk::SShots & shots, uint32_t countShots, double stabilityFactor, const ::gpk::n3f32 & position, const ::gpk::n3f32 & direction, float speedDebris, float brightness, float lifetime)	{
-			if(Delay.Value < Delay.Limit || 0 == countShots)
-				return 0;
-
-			int32_t				indexFirst			= -1;
-			if(countShots)
-				indexFirst		= shots.SpawnForcedDirected(Stability * stabilityFactor, position, direction, speedDebris, brightness, lifetime);
-
-			for(uint32_t iDebris = 0; iDebris < (countShots - 1); ++iDebris)
-				shots.SpawnForcedDirected(Stability * stabilityFactor, position, direction, speedDebris, brightness, lifetime);
-
-			Delay.Value		= 0;
-			Overheat.Value	+= OverheatPerShot;
 			return indexFirst;
 		}
 	};
--- llc/gpk_apod_n3.h
+++ llc/gpk_apod_n3.h
@@ -8,21 +8,22 @@
 namespace gpk
 {
 #pragma pack(push, 1)
-	typedef	::gpk::apod<::gpk::n3char	>	an3c	, an3char		;
-	typedef	::gpk::apod<::gpk::n3uchar	>	an3uc	, an3uchar		;
-	typedef	::gpk::apod<::gpk::n3f32	>	an3f32	;
-	typedef	::gpk::apod<::gpk::n3f64	>	an3f64	;
-	typedef	::gpk::apod<::gpk::n3u8		>	an3u8	;
-	typedef	::gpk::apod<::gpk::n3u16	>	an3u16	;
-	typedef	::gpk::apod<::gpk::n3u32	>	an3u32	;
-	typedef	::gpk::apod<::gpk::n3u64	>	an3u64	;
-	typedef	::gpk::apod<::gpk::n3i8		>	an3i8	;
-	typedef	::gpk::apod<::gpk::n3i16	>	an3i16	;
-	typedef	::gpk::apod<::gpk::n3i32	>	an3i32	;
-	typedef	::gpk::apod<::gpk::n3i64	>	an3i64	;
-
 	template<typename _tDimension>	
 	using		an3	= ::gpk::apod<::gpk::n3<_tDimension>>;
+
+	typedef	::gpk::an3<char	>	an3c	, an3char		;
+	typedef	::gpk::an3<uchar>	an3uc	, an3uchar		;
+	typedef	::gpk::an3<f32	>	an3f32	;
+	typedef	::gpk::an3<f64	>	an3f64	;
+	typedef	::gpk::an3<u8	>	an3u8	;
+	typedef	::gpk::an3<u16	>	an3u16	;
+	typedef	::gpk::an3<u32	>	an3u32	;
+	typedef	::gpk::an3<u64	>	an3u64	;
+	typedef	::gpk::an3<i8	>	an3i8	;
+	typedef	::gpk::an3<i16	>	an3i16	;
+	typedef	::gpk::an3<i32	>	an3i32	;
+	typedef	::gpk::an3<i64	>	an3i64	;
+
 #pragma pack(pop)
 } // namespace
 
